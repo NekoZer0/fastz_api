7fa2b5ddddfaffe74f4f211b1a8ffbcf960eedf1
Jose de Almeida <josedealmeida930@gmail.com>
2026-01-21 12:09:21 +0100

    feat: add update and delete user endpoints with corresponding tests

--- fastz_api/app.py
+++ fastz_api/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fastz_api.schemas import (
     Message,
@@ -35,3 +35,28 @@ def create_user(user: UserSchema):
     )
     database.append(user_with_id)
     return user_with_id
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDBSchema(**user.model_dump(), id=user_id)
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuario não encontrado'
+        )
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Usuario não encontrado'
+        )
+    deleted_user = database.pop(user_id - 1)
+    return deleted_user
--- tests/test_app.py
+++ tests/test_app.py
@@ -30,3 +30,28 @@ def test_read_user(client):
     assert 'users' in response_data
     assert isinstance(response_data['users'], list)
     assert len(response_data['users']) >= 1
+
+
+def test_update_user(client):
+    updated_data = {
+        'username': 'updated_username',
+        'email': 'updated@example.com',
+        'password': 'newsecurepassword123',
+    }
+    response = client.put('/users/1', json=updated_data)
+    assert response.status_code == HTTPStatus.OK
+    response_data = response.json()
+    assert response_data['username'] == updated_data['username']
+    assert response_data['email'] == updated_data['email']
+    assert 'id' in response_data
+    assert 'password' not in response_data
+    assert response_data['id'] == 1
+    assert response_data['username'] == 'updated_username'
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    response_data = response.json()
+    assert response_data['id'] == 1
+    assert response_data['username'] == 'updated_username'
